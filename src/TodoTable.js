d11fd2f0e7597ac5cfaf0681a576ac571675c760
duongthienly <duongthienlee@gmail.com>
2018-03-12 11:20:14 +0200

    Minor adjustment the app.

--- src/TodoTable.js
+++ src/TodoTable.js
@@ -1,42 +1,40 @@
 import React, {Component} from "react"
 import "./App.css"
+
 class TodoTable extends Component {
-  removeItem(item, index) {
-    this.props.removeToDo(item, index)
-  }
-  render() {
-    return (
-      <div className="App">
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Description</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.todos.map((item, index) => (
-              <tr key={index}>
-                <td> {item.date}</td>
-                <td> {item.description}</td>
-                <td>
-                  <button
-                    onClick={() => {
-                      this.removeItem(item, index)
-                    }}
-                    key={index}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th/>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.todos.map((item, index) => (
+                        <tr>
+                            <td> {item.date}</td>
+                            <td> {item.description}</td>
+                            <td>
+                                <button
+                                    id={index}
+                                    onClick={() => {
+                                        this.props.removeToDo(item, index)
+                                    }}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 export default TodoTable
